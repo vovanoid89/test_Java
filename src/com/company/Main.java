2ecb79507f097351d670f9f4378f4f085b9c46df
vovanoid89 <vovanoid89@gmail.com>
2017-10-24 00:16:27 +0300

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Input a");
+        int a = sc.nextInt();
+        System.out.println("Input b");
+        int b = sc.nextInt();
+
+        System.out.println("parameter = "+2*(a+b));;
+        System.out.println("square = "+a*b);
+        sc.close();
+    }
+}
