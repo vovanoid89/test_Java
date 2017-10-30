f23520df0bff872836b3edd085151352c019edf7
vovanoid89 <vovanoid89@gmail.com>
2017-10-30 19:13:03 +0200

    +lesson1_2

--- src/com/company/Lesson1_2.java
+++ src/com/company/Lesson1_2.java
@@ -1,4 +1,18 @@
 package com.company;
+import java.util.Scanner;
 
 public class Lesson1_2 {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Inpur Radius:");
+        int r =sc.nextInt();
+        double pi = 3.14;
+
+        System.out.println("L= "+2*pi*r);
+        System.out.println ("S= "+pi*r*r);
+
+
+    }
 }
