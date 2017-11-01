f3e19385a20d0247a2cbcebaf42b3c13e347352f
vovanoid89 <vovanoid89@gmail.com>
2017-11-01 18:35:40 +0200

    +lesson1_4

---
+++ src/com/company/Lesson1_4.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+public class Lesson1_4 {
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Input cordinate 1:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Input cordinate 2:");
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        double e = sqrt (c-a)*(d-b);
+        System.out.println("Average="+c);
+
+    Math.
+}
